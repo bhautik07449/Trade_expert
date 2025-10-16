88f384a11651d3b0cc72e6b4d939cb65f9c89e58
bhautik07449 <bhautikpokal7449@gmail.com>
2025-10-16 14:43:06 +0530

    changes

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ImageSlider from '../commonUI/image-slider'
+import ImageSlider from '../commonUI/ImageSlider'
 import CardUi from '../commonUI/Card-Ui'
 import SpotMarketTable from '../commonUI/spot-market'
 import InquiryDialog from '../componenet/Dialog/inquiry-dialog'
