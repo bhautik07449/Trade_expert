e3dba646481b3faeb90d92632cb8a748486c4643
bhautik07449 <bhautikpokal7449@gmail.com>
2026-02-05 22:47:52 +0530

    our process set in responsive

--- src/component/OurProcess.tsx
+++ src/component/OurProcess.tsx
@@ -1,11 +1,30 @@
-import { Box } from "@mui/system"
-import Title from "../commonUI/labelTitle"
+import { Box } from "@mui/system";
+import Title from "../commonUI/labelTitle";
 
 export default function OurProcess() {
     return (
-        <Box sx={{ my: 4, textAlign: "center" }}>
+        <Box
+            sx={{
+                my: { xs: 3, md: 6 },
+                px: { xs: 2, sm: 3 },
+                textAlign: "center",
+            }}
+        >
             <Title title="Our" label=" Process" />
-            <img src="https://sourceseas.itcoders.in/img/front-end/our_prosess.png" alt="Our Process" />
+
+            <Box
+                component="img"
+                src="https://sourceseas.itcoders.in/img/front-end/our_prosess.png"
+                alt="Our Process"
+                sx={{
+                    width: "100%",
+                    maxWidth: 1100,
+                    height: "auto",
+                    mt: { xs: 2, md: 4 },
+                    mx: "auto",
+                    display: "block",
+                }}
+            />
         </Box>
-    )
+    );
 }
