4016818b487c4a4f908f145ed2c6b2d040c74359
bhautik07449 <bhautikpokal7449@gmail.com>
2026-02-05 22:43:37 +0530

    changes

--- src/pages/AboutUs.tsx
+++ src/pages/AboutUs.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import Title from "../commonUI/labelTitle"
 import OurView from "../component/Ourview";
 
