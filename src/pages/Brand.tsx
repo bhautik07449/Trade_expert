7288964f2defa658a83efa1d873ca65ea4bea958
bhautik07449 <bhautikpokal7449@gmail.com>
2026-03-14 01:41:34 +0530

    brand api set

--- src/pages/Brand.tsx
+++ src/pages/Brand.tsx
@@ -1,23 +1,61 @@
 import { Box, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
+import { toast } from "react-toastify";
+import Brandservice from "../service/brand.service";
+
+interface Props {
+    logo: string,
+    name: string,
+    description: string,
+    category: category
+}
+
+interface category {
+    name: string
+}
 
 export default function Brand() {
+    const [list, setList] = useState<Props[]>([])
+    console.log("list", list);
+
+    const getList = async () => {
+        try {
+            const res = await Brandservice.getList()
+            if (res) {
+                setList(res?.data)
+            }
+        } catch (error) {
+            toast.error("bran not found")
+        }
+    }
+
+    useEffect(() => {
+        getList()
+    }, [])
+
     return (
         <Box sx={{ bgcolor: 'white', minHeight: '100vh', pb: 10 }}>
             <img src="https://sourceseas.itcoders.in/img/front-end/brands.jpg" alt="Brands" style={{ width: '100%', minHeight: '200px', maxHeight: '400px', }} />
-            <Box sx={{ p: 2, textAlign: 'center' }}>
-                <Typography variant="h5" component="h2" sx={{ fontWeight: 600, color: 'secondary.main' }}>
-                    Agri & Foods
-                </Typography>
-            </Box>
+            <Box sx={{ display: "flex", flexDirection: "column", gap: "20px" }}>
+                {list?.map((item) => (
+                    <>
+                        <Box sx={{ p: 2, textAlign: 'center' }}>
+                            <Typography variant="h5" component="h2" sx={{ fontWeight: 600, color: 'secondary.main' }}>
+                                {item?.category?.name}
+                            </Typography>
+                        </Box>
 
-            <Box sx={{ maxWidth: "1100px", mx: "auto", px: 2, display: "flex", gap: 4, flexWrap: "wrap", alignItems: "center", justifyContent: "center", textAlign: "center" }}>
-                <img src="https://sourceseas.itcoders.in/files/brand/logo/3/logo.png" alt="FSSAI Approved Foods" style={{ width: '100px', height: "100px" }} />
-                <Box>
-                    <Typography variant="h6" sx={{ color: 'secondary.main', mb: 4 }}>
-                        FLAVOURICA
-                    </Typography>
-                    <Typography>Inspired by flavours,Flavourica is registered brand of Sourceseas overseas which is house of mouthwatering aroma and flavoured items made using natural colors and ingredients incepted for enrich peoples's taste globally.Flavourica having totally uniq range of flavoured items like aromatic ingredient,instant ready to eat, snacks,sweets,dryfruits,chocolates in world-class quality.</Typography>
-                </Box>
+                        <Box sx={{ maxWidth: "1100px", mx: "auto", px: 2, display: "flex", gap: 4, flexWrap: "wrap", flexDirection: "column", alignItems: "center", justifyContent: "center", textAlign: "center" }}>
+                            <img src={item?.logo} alt="FSSAI Approved Foods" style={{ width: '100px', height: "100px" }} />
+                            <Box>
+                                <Typography variant="h6" sx={{ color: 'secondary.main', mb: 4 }}>
+                                    {item?.name}
+                                </Typography>
+                                <Typography>{item?.description}</Typography>
+                            </Box>
+                        </Box>
+                    </>
+                ))}
             </Box>
         </Box >
     )
---
+++ src/service/brand.service.jsx
@@ -0,0 +1,16 @@
+import serverCall from "../serverCall";
+
+const getList = async () => {
+    try {
+        const response = serverCall.get('/brands')
+        return response
+    } catch (error) {
+        throw error
+    }
+}
+
+const Brandservice = {
+    getList
+};
+
+export default Brandservice;
