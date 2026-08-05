b5940148e43d334932baccdd070aa67959cd7b99
bhautik07449 <bhautikpokal7449@gmail.com>
2026-08-05 16:46:28 +0530

    footer company name change

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -64,7 +64,7 @@ const Footer = () => {
             Concept designed and maintained by - TDS (the_digital_salon)
           </Typography> */}
         <Typography variant="body2">
-          © 2025 All rights reserved by Sourceseas Overseas Private Limited
+          © 2025 All rights reserved by MM, a Standard of FiatCo initiative Private Limited
         </Typography>
       </Box>
     </>
